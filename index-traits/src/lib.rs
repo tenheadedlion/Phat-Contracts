d3189a101f5b7ba88fdf653d89ebd2a2959690f1
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-10-13 11:59:42 +0800

    add index-traits

---
+++ index-traits/src/lib.rs
@@ -0,0 +1,43 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+use ink_prelude::vec::Vec;
+use scale::{Decode, Encode};
+
+#[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum SignedTransaction {
+    /// Ethereum signed transaction
+    EthSignedTX,
+    /// Substrate-based chain signed transaction
+    SubSignedTX,
+}
+
+#[ink::trait_definition]
+pub trait Signer {
+    /// Sign a transaction
+    #[ink(message)]
+    fn sign_transaction(&self, unsigned_tx: Vec<u8>) -> SignedTransaction;
+}
+
+#[ink::contract]
+mod stub {
+    use super::*;
+
+    #[ink(storage)]
+    pub struct StubSigner;
+
+    impl StubSigner {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            StubSigner {}
+        }
+    }
+
+    impl Signer for StubSigner {
+        #[ink(message)]
+        fn sign_transaction(&self, unsigned_tx: Vec<u8>) -> SignedTransaction {
+            SignedTransaction::SubSignedTX
+        }
+    }
+}
