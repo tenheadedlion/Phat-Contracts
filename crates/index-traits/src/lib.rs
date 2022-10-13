8f2db8a8b701e9728dc47e79a03ce04d1a82f6a4
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-10-13 21:29:55 +0800

    add signers

--- crates/index-traits/src/lib.rs
+++ crates/index-traits/src/lib.rs
@@ -1,17 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use ink_env::AccountId;
 use ink_lang as ink;
 use ink_prelude::vec::Vec;
 use scale::{Decode, Encode};
-use ink_env::AccountId;
 
 #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum SignedTransaction {
     /// Ethereum signed transaction
-    EthSignedTX,
+    EthSignedTX(Vec<u8>),
     /// Substrate-based chain signed transaction
-    SubSignedTX,
+    SubSignedTX(Vec<u8>),
 }
 
 #[ink::trait_definition]
@@ -21,33 +21,19 @@ pub trait Signer {
     fn sign_transaction(&self, unsigned_tx: Vec<u8>) -> SignedTransaction;
 }
 
-pub fn greet() {
-
+pub trait ToArray<T, const N: usize> {
+    fn to_array(&self) -> [T; N];
 }
 
-
-
-/*
-#[ink::contract]
-mod stub {
-    use super::*;
-
-    #[ink(storage)]
-    pub struct StubSigner;
-
-    impl StubSigner {
-        #[ink(constructor)]
-        pub fn new() -> Self {
-            StubSigner {}
+impl<T, const N: usize> ToArray<T, N> for Vec<T>
+where
+    T: Default + Copy,
+{
+    fn to_array(&self) -> [T; N] {
+        let mut arr = [T::default(); N];
+        for (a, v) in arr.iter_mut().zip(self.iter()) {
+            *a = *v;
         }
-    }
-
-    impl Signer for StubSigner {
-        #[ink(message)]
-        fn sign_transaction(&self, unsigned_tx: Vec<u8>) -> SignedTransaction {
-            SignedTransaction::SubSignedTX
-        }
+        arr
     }
 }
-
-*/
