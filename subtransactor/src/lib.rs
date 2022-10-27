7e3f345c514d7c517e0b1fe8f2cc657cdc8be80d
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-10-27 23:15:01 +0800

    chore

--- subtransactor/src/lib.rs
+++ subtransactor/src/lib.rs
@@ -13,24 +13,9 @@ mod sub_transactor {
     use ink_storage::traits::{PackedLayout, SpreadLayout};
     use paralib::ToArray;
     use pink::{chain_extension::signing::sign, http_post, PinkEnvironment};
+    use pink_web3::transports::resolve_ready;
     use primitive_types::H256;
     use scale::{Decode, Encode};
-    use serde_json::{json, Value};
-
-    #[derive(Debug, PartialEq, Eq, Encode, Decode)]
-    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
-    pub enum Error {
-        InvalidBody,
-        InvalidUrl,
-        InvalidSignature,
-        RequestFailed,
-        NoPermissions,
-        ApiKeyNotSet,
-        ChainNotConfigured,
-        InvalidAccount,
-    }
-
-    pub type Result<T> = core::result::Result<T, Error>;
 
     #[ink(storage)]
     pub struct SubTransactor {
@@ -44,124 +29,10 @@ mod sub_transactor {
                 rpc_node: "http://localhost:9933".into(),
             }
         }
-        /*
-                #[ink(message)]
-                pub fn send_transaction(&self, chain: String, tx_hash: Vec<u8>) -> Result<String> {
-                    let tx_hex = hex::encode(&tx_hash);
 
-                    let data = format!(
-                        r#"{{"id":1,"jsonrpc":"2.0","method":"author_submitExtrinsic","params":["{}"]}}"#,
-                        tx_hex
-                    )
-                    .into_bytes();
-                    let resp_body = call_rpc(&self.rpc_node, data)?;
-                    let body = String::from_utf8(resp_body).unwrap();
-
-                    Ok(body)
-                }
-                /// Sends message to the target EVM contract
-                #[ink(message)]
-                pub fn test(&self) {
-                    let pallet_index: u8 = 0;
-                    let call_index: u8 = 1;
-                    let signer = sp_keyring::AccountKeyring::Alice;
-                    let account = signer.to_account_id();
-                    let signer_nonce = get_nonce(&account);
-
-                    let call = (pallet_index, call_index, hex!("284772656574696e677321"));
-                    let extra = (
-                        Era::Immortal,
-                        Compact(signer_nonce),
-                        Compact(500000000000000u128),
-                    );
-
-                    let runtime_version = get_runtime_version().await;
-                    let genesis_hash = get_genesis_hash().await;
-
-                    let additional = (
-                        runtime_version.spec_version,
-                        runtime_version.transaction_version,
-                        genesis_hash,
-                        genesis_hash,
-                    );
-                    let signature = {
-                        // Combine this data together and SCALE encode it:
-                        let full_unsigned_payload = (&call, &extra, &additional);
-                        let full_unsigned_payload_scale_bytes = full_unsigned_payload.encode();
-
-                        // If payload is longer than 256 bytes, we hash it and sign the hash instead:
-                        if full_unsigned_payload_scale_bytes.len() > 256 {
-                            AccountKeyring::Alice.sign(&blake2_256(&full_unsigned_payload_scale_bytes)[..])
-                        } else {
-                            AccountKeyring::Alice.sign(&full_unsigned_payload_scale_bytes)
-                        }
-                    };
-                    let signature_to_encode = Some((
-                        //signer.to_h256_public(),
-                        MultiAddress::Id::<_, u32>(account),
-                        // The actual signature, computed above:
-                        MultiSignature::Sr25519(signature),
-                        // Extra information to be included in the transaction:
-                        extra,
-                    ));
-                    let payload_scale_encoded = encode_extrinsic(signature_to_encode, call);
-                    let payload_hex = format!("0x{}", hex::encode(&payload_scale_encoded));
-
-                    // Submit it!
-                    println!("Submitting this payload: {}", &payload_hex);
-                    let res = rpc(&self.rpc_node, "author_submitExtrinsic", &payload_hex)?;
-                    // The result from this call is the hex value for the extrinsic hash.
-                    println!("{:?}", res);
-                }
-        */
         #[ink(message)]
-        pub fn get_genesis_hash(&self)
-        // -> Result<H256>
-        {
-            let genesis_hash_json = rpc(&self.rpc_node, "chain_getBlockHash", [0]).unwrap();
-            dbg!(&genesis_hash_json["result"]);
-            //let genesis_hash_hex = genesis_hash_json.as_str()?;
-            //H256::from_str(genesis_hash_hex)
+        pub fn get_genesis_hash(&self) {
         }
-    }
-
-    fn rpc<Params: serde::Serialize>(
-        rpc_node: &str,
-        method: &str,
-        params: Params,
-    ) -> Result<Value> {
-        //let content_length = format!("{}", params.len());
-
-        //let a = serde::t
-
-        //let data = format!(
-        //    r#"{{"id":1,"jsonrpc":"2.0","method":"{}","params":["{}"]}}"#,
-        //    method, params
-        //)
-        //.into_bytes();
-        let data = json! {{
-            "id": 1,
-            "jsonrpc": "2.0",
-            "method": method,
-            "params": params
-        }};
-        let data = data.to_string();
-        dbg!(&data);
-        let content_length = data.len();
-
-        let headers: Vec<(String, String)> = vec![
-            ("Content-Type".into(), "application/json".into()),
-            ("Content-Length".into(), content_length.to_string()),
-        ];
-        //
-        let response = http_post!(rpc_node, data.as_bytes(), headers);
-        //
-        if response.status_code != 200 {
-            return Err(Error::RequestFailed);
-        }
-
-        let body: Value = serde_json::from_slice(&response.body).map_err(|_| Error::InvalidBody)?;
-        Ok(body)
     }
 
     #[cfg(test)]
